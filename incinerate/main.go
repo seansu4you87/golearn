151de107f2809e3b334f85c2c90582b00f835e61
Sean Yu <seansu4you87@gmail.com>
2014-03-17 00:19:46 -0700

    Adding tests for multiple gophers/piles/incinerators

--- incinerate/main.go
+++ incinerate/main.go
@@ -1,10 +1,5 @@
 package main
 
-import (
-	// "fmt"
-	"time"
-)
-
 /* Book */
 type Book struct {
 }
@@ -14,29 +9,60 @@ type Named interface {
 }
 
 func main() {
-	books := []Book{}
-	for i := 0; i < 10; i++ {
-		books = append(books, Book{})
+	books1 := []Book{}
+	books2 := []Book{}
+	for i := 0; i < 5; i++ {
+		books1 = append(books1, Book{})
+		books2 = append(books2, Book{})
 	}
 
-	p := NewPile("P-1", books)
-	i := NewIncinerator("I-1")
+	p1 := NewPile("P-1", books1)
+	p2 := NewPile("P-2", books2)
+	i1 := NewIncinerator("I-1")
+	i2 := NewIncinerator("I-2")
 
 	g1 := &Gopher{
 		name:        "Al",
-		Pile:        p,
-		Incinerator: i,
+		Pile:        p1,
+		Incinerator: i1,
 	}
 
 	g2 := &Gopher{
 		name:        "Bob",
-		Pile:        p,
-		Incinerator: i,
+		Pile:        p2,
+		Incinerator: i2,
+	}
+
+	g3 := &Gopher{
+		name:        "Chad",
+		Pile:        p1,
+		Incinerator: i1,
+	}
+
+	g4 := &Gopher{
+		name:        "Dan",
+		Pile:        p2,
+		Incinerator: i2,
+	}
+
+	g5 := &Gopher{
+		name:        "Ed",
+		Pile:        p1,
+		Incinerator: i1,
+	}
+
+	g6 := &Gopher{
+		name:        "Frank",
+		Pile:        p2,
+		Incinerator: i2,
 	}
 
 	go g1.Work()
-	time.Sleep(9 * time.Second)
 	go g2.Work()
+	go g3.Work()
+	go g4.Work()
+	go g5.Work()
+	go g6.Work()
 
 	select {
 	// default:
